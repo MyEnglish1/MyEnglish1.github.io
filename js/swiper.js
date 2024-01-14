cda310808cbfab0b1b576df83ae3c22d46cb855b
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2024-01-14 16:31:29 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -98,7 +98,7 @@ Array.from(playButtons).forEach(function(button) {
             return voice.lang === 'en-GB' && voice.gender === 'male';
         }); // Установим голос на британский мужской
 
-        utterance.rate = 1.1; // Установим скорость речи
+        utterance.rate = 1; // Установим скорость речи
 
         utterance.pitch = 1; // Установим тон речи
 
