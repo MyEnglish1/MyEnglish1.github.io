73dbcd1643f4b8584765f019ff123d3fb5419e93
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2024-01-20 13:04:26 +0300

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,169 @@
+
+
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      // меняем точки будеты на цифры и вместо bullets пишем fraction
+      type: 'fraction',
+      // clickable: true,
+      // dynamicBullets: true,
+  },
+  initialSlide: 0,
+  grabCursor: true,
+  // сколько выводить слайдеров
+  slidesPerView: 3,
+  // отступы между слайдами
+  spaceBetween: 30,
+  // количество пролистываемых слайдов
+  slidesPerGroup: 1,
+  // бесконечный слайдер
+  // loop: true,
+  // кол-во дублирующих слайдов
+  // loopedSlides:3,
+  // скорость прокрутки
+
+  
+  speed: 1600,
+  // автопрокрутка
+  // autoplay: {
+  //   // пауза между прокруткой
+  //   delay: 3000,
+  //   // заканчивать на последнем слайде
+  //   stopOnLastSlide: true,
+  //   // отключение после ручного переключения 
+  //   disableOnInteraction: false,
+  // },
+
+  breakpoints: {
+  
+    320: {
+      slidesPerView: 1,
+    },
+
+    480: {
+      slidesPerView: 2,
+    },
+
+    1200: {
+      slidesPerView: 3,
+    },
+
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+    
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  
+
+});
+
+
+
+var playButtons = document.getElementsByClassName('button');
+
+Array.from(playButtons).forEach(function(button) {
+    button.addEventListener('click', function() {
+        var card = button.closest('.card');
+        var textBlock = card.querySelector('.stages__item_content_en');
+        var text = textBlock.innerText;
+
+        var repeatCount = 10; // Количество повторений
+        var delay = 2500; // Задержка в миллисекундах между повторениями
+
+        var utterance = new SpeechSynthesisUtterance(text);
+
+        utterance.lang = 'en-GB'; // Установим язык речи на британский английский
+
+        utterance.voice = speechSynthesis.getVoices().find(function(voice) {
+            return voice.lang === 'en-GB' && voice.gender === 'male';
+        }); // Установим голос на британский мужской
+
+        utterance.rate = 0.9; // Установим скорость речи
+
+        utterance.pitch = 1; // Установим тон речи
+
+        utterance.volume = 1; // Установим громкость
+
+        var repeat = function(count) {
+            if (count <= 0) return;
+            
+            window.speechSynthesis.speak(utterance);
+            
+            setTimeout(function() {
+                repeat(count - 1);
+            }, delay);
+        };
+
+        repeat(repeatCount);
+    });
+});
+
+
+
+
+
+
+
+
+// Получить ссылки на элементы
+var headerTitleChange = document.querySelector('.lesson__header_title_change');
+var swiperWrapper = document.querySelector('.swiper-wrapper');
+
+// Обработчик события клика на заголовок
+headerTitleChange.addEventListener('click', function() {
+  // Получить все слайды
+  var slides = swiperWrapper.querySelectorAll('.swiper-slide');
+
+  // Перемешать слайды в случайном порядке
+  var shuffledSlides = Array.from(slides).sort(function() { return Math.random() - 0.5; });
+
+  // Очистить контейнер слайдов
+  swiperWrapper.innerHTML = '';
+
+  // Добавить перемешанные слайды обратно в контейнер
+  shuffledSlides.forEach(function(slide) {
+    swiperWrapper.appendChild(slide);
+  });
+});
+
+
+
+
+
+
+
+
+document.querySelector('.lesson__header_title_ru').addEventListener('click', function() {
+  document.querySelectorAll('.stages__item_content_ru').forEach(function(element) {
+      element.style.display = 'block';
+  });
+  
+  document.querySelectorAll('.stages__item_content_en').forEach(function(element) {
+      element.style.display = 'none';
+  });
+});
+
+document.querySelector('.lesson__header_title_en').addEventListener('click', function() {
+  document.querySelectorAll('.stages__item_content_ru').forEach(function(element) {
+      element.style.display = 'none';
+  });
+  
+  document.querySelectorAll('.stages__item_content_en').forEach(function(element) {
+      element.style.display = 'block';
+  });
+});
+
+
