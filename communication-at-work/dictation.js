1f056e5a0b7ef45c92b2416e1cdb0ff273eb5ab5
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2024-09-30 13:09:37 +0300

    Add files via upload

---
+++ communication-at-work/dictation.js
@@ -0,0 +1,54 @@
+const words = {
+    "новый выпуск": "update",
+    "хорошо": "well",
+    "общительный": "outgoing",
+    "кто-нибудь": "anyone",
+    "что-нибудь": "anything",
+    "надежный": "reliable",
+    "глупый, тупой": "stupid",
+    "организованный": "organized",
+    "быстрый, скорый": "fast",
+    "трудный, утомительный, тяжелый": "hard",
+    "робкий, стеснительный": "shy",
+    "достойный, добропорядочный": "good",
+    "для, ради": "for",
+    "удивляться, дивиться": "wonder",
+    "заставать врасплох": "surprise",
+    "беспокоиться, волноваться": "worry",
+    "говорить, сказать, произносить": "say",
+    "причина": "reason",
+    "рассказывать, рассказать": "tell",
+    "добавочный, дальнейший": "further",
+    "ожидать, надеяться, предполагать": "expect",
+    "наем, прокат": "hire",
+    "честный": "honest",
+    "пункт, параграф, статья": "item",
+    "с, начиная с, с тех пор как": "since",
+    "суммировать, подводить итог": "summarize",
+    "повестка дня": "agenda",
+    "амбициозный": "ambitious",
+    "умный, сметливый": "clever"
+  };
+  
+  let currentValue;
+  
+  function getRandomWord() {
+    const values = Object.keys(words);
+    currentValue = values[Math.floor(Math.random() * values.length)];
+    document.getElementById('wordDisplay').textContent = currentValue; // Выводим значение
+  }
+  
+  function checkAnswer() {
+    const userInput = document.getElementById('userInput').value.trim();
+    if (userInput === words[currentValue]) {
+  
+        getRandomWord(); // Получаем новое значение
+    } else {
+        alert('Неправильно! Правильный ответ: ' + words[currentValue]);
+    }
+    document.getElementById('userInput').value = ''; // Очищаем поле ввода
+  }
+  
+  // Инициализируем игру
+  getRandomWord(); // Получаем первое случайное значение
+  
---
+++ communication-at-work/dist/dictation.dev.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var words = {
+  "новый выпуск": "update",
+  "хорошо": "well",
+  "общительный": "outgoing",
+  "кто-нибудь": "anyone",
+  "что-нибудь": "anything",
+  "надежный": "reliable",
+  "глупый, тупой": "stupid",
+  "организованный": "organized",
+  "быстрый, скорый": "fast",
+  "трудный, утомительный, тяжелый": "hard",
+  "робкий, стеснительный": "shy",
+  "достойный, добропорядочный": "good",
+  "для, ради": "for",
+  "удивляться, дивиться": "wonder",
+  "заставать врасплох": "surprise",
+  "беспокоиться, волноваться": "worry",
+  "говорить, сказать, произносить": "say",
+  "причина": "reason",
+  "рассказывать, рассказать": "tell",
+  "добавочный, дальнейший": "further",
+  "ожидать, надеяться, предполагать": "expect",
+  "наем, прокат": "hire",
+  "честный": "honest",
+  "пункт, параграф, статья": "item",
+  "с, начиная с, с тех пор как": "since",
+  "суммировать, подводить итог": "summarize",
+  "повестка дня": "agenda",
+  "амбициозный": "ambitious",
+  "умный, сметливый": "clever"
+};
+var currentValue;
+
+function getRandomWord() {
+  var values = Object.keys(words);
+  currentValue = values[Math.floor(Math.random() * values.length)];
+  document.getElementById('wordDisplay').textContent = currentValue; // Выводим значение
+}
+
+function checkAnswer() {
+  var userInput = document.getElementById('userInput').value.trim();
+
+  if (userInput === words[currentValue]) {
+    getRandomWord(); // Получаем новое значение
+  } else {
+    alert('Неправильно! Правильный ответ: ' + words[currentValue]);
+  }
+
+  document.getElementById('userInput').value = ''; // Очищаем поле ввода
+} // Инициализируем игру
+
+
+getRandomWord(); // Получаем первое случайное значение
