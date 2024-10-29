bb9e13cf4037fb6cdf497f7e72c861955dc2a213
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2024-10-29 11:25:43 +0300

    Add files via upload

--- welcome/dictation.js
+++ welcome/dictation.js
@@ -32,57 +32,50 @@ const words = {
   };
 
   let currentValue;
-
+  let correctCount = 0;
+  let wrongCount = 0;
+  
   function getRandomWord() {
       const values = Object.keys(words);
       currentValue = values[Math.floor(Math.random() * values.length)];
       document.getElementById('wordDisplay').textContent = currentValue;
   }
-
+  
   function checkAnswer() {
       const userInput = document.getElementById('userInput').value.trim();
       const popup = document.getElementById('popup');
       const popupMessage = document.getElementById('popupMessage');
       const popupImg = document.getElementById('popupImg');
-
+  
       if (userInput === words[currentValue]) {
+          // Правильный ответ
+          correctCount++;
+          document.querySelector('.lesson__header_right').textContent = correctCount;
+  
           popupMessage.textContent = '';
           popupImg.src = 'correct-image-url.jpg'; // Замените на URL вашей картинки
           popup.style.display = 'block';
-
+  
           setTimeout(() => {
               popup.style.display = 'none';
           }, 1000); // Закрываем всплывающее окно через 1 секунду
-
+  
           getRandomWord();
       } else {
-          alert('Неправильно! Правильный ответ: ' + words[currentValue]);
+          // Неправильный ответ
+          wrongCount++;
+          document.querySelector('.lesson__header_wrong').textContent = wrongCount;
+  
+         
+                      alert('Неправильно! Правильный ответ: ' + words[currentValue]);
+  
+          setTimeout(() => {
+              popup.style.display = 'none';
+          }, 1000); // Закрываем всплывающее окно через 1 секунду
+  
+          getRandomWord();
       }
       document.getElementById('userInput').value = '';
   }
-
+  
   getRandomWord();
-  
-  
-  // let currentValue;
-  
-  // function getRandomWord() {
-  //   const values = Object.keys(words);
-  //   currentValue = values[Math.floor(Math.random() * values.length)];
-  //   document.getElementById('wordDisplay').textContent = currentValue; // Выводим значение
-  // }
-  
-  // function checkAnswer() {
-  //   const userInput = document.getElementById('userInput').value.trim();
-  //   if (userInput === words[currentValue]) {
-  
-  //       getRandomWord(); // Получаем новое значение
-  //   } else {
-  //       alert('Неправильно! Правильный ответ: ' + words[currentValue]);
-  //   }
-  //   document.getElementById('userInput').value = ''; // Очищаем поле ввода
-  // }
-  
-  // // Инициализируем игру
-  // getRandomWord(); // Получаем первое случайное значение
-  
