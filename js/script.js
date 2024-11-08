094ef0cd4f2ec9cc9263742515d90a7819502f61
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2024-11-08 10:19:56 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,90 +1,195 @@
-const stagesItemContentRuElements = document.querySelectorAll('.stages__item_content_ru');
-const stagesItemContentENElements = document.querySelectorAll('.stages__item_content_en');
+// const stagesItemContentRuElements = document.querySelectorAll('.stages__item_content_ru');
+// const stagesItemContentENElements = document.querySelectorAll('.stages__item_content_en');
 
-stagesItemContentRuElements.forEach(element => {
-    const newElement = document.createElement('div');
-    newElement.classList.add('stages__item_content_img');
-    newElement.innerHTML = '<img src="../../img/update-alt.svg" alt="img">';
-    element.insertAdjacentElement('afterend', newElement);
-});
+// stagesItemContentRuElements.forEach(element => {
+//   const newElement = document.createElement('div');
+//   newElement.classList.add('stages__item_content_img');
+//   newElement.innerHTML = '<img src="../../img/update-alt.svg" alt="img">';
+//   element.insertAdjacentElement('afterend', newElement);
+// });
+
+// let currentPhraseKey;
+// let currentPhraseValue;
+// let usedWords = []; // Array to store used words
+
+// function loadNextPhrase() {
+//   const keys = Object.keys(phrases);
+//   const randomIndex = Math.floor(Math.random() * keys.length);
+//   currentPhraseKey = keys[randomIndex];
+//   currentPhraseValue = phrases[currentPhraseKey];
+
+//   document.querySelector('.test__phrase_ru').innerText = currentPhraseValue;
+//   displayWords(currentPhraseKey);
+// }
+
+// function displayWords(phrase) {
+//   const words = phrase.split(' ');
+//   const wrapper = document.querySelector('.test__wrapper');
+//   wrapper.innerHTML = '';
+
+//   words.sort(() => Math.random() - 0.5).forEach(word => {
+//     const wordDiv = document.createElement('div');
+//     wordDiv.className = 'test__word';
+//     wordDiv.innerText = word;
+//     wordDiv.onclick = () => addWord(wordDiv, word);
+//     wrapper.appendChild(wordDiv);
+//   });
+// }
+
+// function addWord(wordDiv, word) {
+//   const phraseDiv = document.querySelector('.test__phrase_en');
+//   phraseDiv.innerText += (phraseDiv.innerText.length > 0 ? ' ' : '') + word;
+//   wordDiv.remove();
+//   checkAnswer();
+//   usedWords.push(word); // Store used words for "Назад"
+// }
+
+// function goBack() {
+//   const wrapper = document.querySelector('.test__wrapper');
+//   if (usedWords.length > 0) {
+//     const lastWord = usedWords.pop(); // Get the last used word
+//     const wordDiv = document.createElement('div');
+//     wordDiv.className = 'test__word';
+//     wordDiv.innerText = lastWord;
+//     wordDiv.onclick = () => addWord(wordDiv, lastWord);
+//     wrapper.appendChild(wordDiv);
+
+//     const phraseDiv = document.querySelector('.test__phrase_en');
+//     phraseDiv.innerText = phraseDiv.innerText.split(' ').slice(0, -1).join(' '); // Remove the last added word
+//   }
+// }
+
+// function checkAnswer() {
+//   const currentAnswer = document.querySelector('.test__phrase_en').innerText.trim();
+
+//   if (currentAnswer.split(' ').length === currentPhraseKey.split(' ').length) {
+//     setTimeout(() => {
+//       if (currentAnswer === currentPhraseKey) {
+//         document.querySelector('.lesson__header_right').innerText =
+//           parseInt(document.querySelector('.lesson__header_right').innerText) + 1;
+//         document.querySelector('#popup').style.display = 'block';
+//         setTimeout(() => {
+//           document.querySelector('#popup').style.display = 'none';
+//         }, 1000);
+//         document.querySelector('.test__phrase_en').innerText = '';
+//       } else {
+//         document.querySelector('.lesson__header_wrong').innerText =
+//           parseInt(document.querySelector('.lesson__header_wrong').innerText) + 1;
+//         alert("Неверно! Правильная фраза: \"" + currentPhraseKey + "\"");
+//         document.querySelector('.test__phrase_en').innerText = '';
+//         displayWords(currentPhraseKey);
+//       }
+//       loadNextPhrase();
+//     }, 500);
+//   }
+// }
+
+// // Event listener for "Назад" button click
+// document.querySelector('.test__ago').onclick = goBack;
+
+// // Инициализация 
+// loadNextPhrase();
+
 
 let currentPhraseKey;
 let currentPhraseValue;
+let usedWords = []; // Store used words
 
 function loadNextPhrase() {
-    const keys = Object.keys(phrases);
-    const randomIndex = Math.floor(Math.random() * keys.length);
-    currentPhraseKey = keys[randomIndex];
-    currentPhraseValue = phrases[currentPhraseKey];
+  const keys = Object.keys(phrases);
+  const randomIndex = Math.floor(Math.random() * keys.length);
+  currentPhraseKey = keys[randomIndex];
+  currentPhraseValue = phrases[currentPhraseKey];
 
-    document.querySelector('.test__phrase_ru').innerText = currentPhraseValue;
-    displayWords(currentPhraseKey);
+  document.querySelector('.test__phrase_ru').innerText = currentPhraseValue;
+  displayWords(currentPhraseKey);
+  updateGoBackButton(); // Update button state
 }
 
 function displayWords(phrase) {
-    const words = phrase.split(' ');
-    const wrapper = document.querySelector('.test__wrapper');
-    wrapper.innerHTML = '';
+  const words = phrase.split(' ');
+  const wrapper = document.querySelector('.test__wrapper');
+  wrapper.innerHTML = '';
 
-    words.sort(() => Math.random() - 0.5).forEach(word => {
-        const wordDiv = document.createElement('div');
-        wordDiv.className = 'test__word';
-        wordDiv.innerText = word;
-        wordDiv.onclick = () => addWord(wordDiv, word);
-        wrapper.appendChild(wordDiv);
-    });
+  words.sort(() => Math.random() - 0.5).forEach(word => {
+    const wordDiv = document.createElement('div');
+    wordDiv.className = 'test__word';
+    wordDiv.innerText = word;
+    wordDiv.onclick = () => addWord(wordDiv, word);
+    wrapper.appendChild(wordDiv);
+  });
 }
 
 function addWord(wordDiv, word) {
-    const phraseDiv = document.querySelector('.test__phrase_en');
-    phraseDiv.innerText += (phraseDiv.innerText.length > 0 ? ' ' : '') + word;
-    wordDiv.remove(); 
-    checkAnswer(); 
+  const phraseDiv = document.querySelector('.test__phrase_en');
+  phraseDiv.innerText += (phraseDiv.innerText.length > 0 ? ' ' : '') + word;
+  usedWords.push(word); // Track added words
+  wordDiv.remove();
+  checkAnswer();
+  updateGoBackButton(); // Update button state
 }
 
+function goBack() {
+  const phraseDiv = document.querySelector('.test__phrase_en');
+  const wrapper = document.querySelector('.test__wrapper');
+
+  if (usedWords.length > 0) {
+    const lastWord = usedWords.pop(); // Get the last word
+    const wordDiv = document.createElement('div');
+    wordDiv.className = 'test__word';
+    wordDiv.innerText = lastWord;
+    wordDiv.onclick = () => addWord(wordDiv, lastWord);
+    wrapper.appendChild(wordDiv);
+
+    phraseDiv.innerText = phraseDiv.innerText.split(' ').slice(0, -1).join(' '); // Remove last added word
+    updateGoBackButton(); // Update button state
+  }
+}
+
+// Check answer logic here
 function checkAnswer() {
-    const currentAnswer = document.querySelector('.test__phrase_en').innerText.trim();
+  const currentAnswer = document.querySelector('.test__phrase_en').innerText.trim();
 
-    if (currentAnswer.split(' ').length === currentPhraseKey.split(' ').length) {
+  if (currentAnswer.split(' ').length === currentPhraseKey.split(' ').length) {
+    setTimeout(() => {
+      if (currentAnswer === currentPhraseKey) {
+        document.querySelector('.lesson__header_right').innerText =
+          parseInt(document.querySelector('.lesson__header_right').innerText) + 1;
+        document.querySelector('#popup').style.display = 'block';
         setTimeout(() => {
-            if (currentAnswer === currentPhraseKey) {
-                document.querySelector('.lesson__header_right').innerText = 
-                  parseInt(document.querySelector('.lesson__header_right').innerText) + 1;
-                // document.querySelector('#popupMessage').innerText = "Правильно! " + currentPhraseKey;
-                document.querySelector('#popup').style.display = 'block';
-                
-                setTimeout(() => {
-                    document.querySelector('#popup').style.display = 'none';
-                }, 1000); // Скрываем через 1 секунду
-
-                document.querySelector('.test__phrase_en').innerText = '';
-            } else {
-                document.querySelector('.lesson__header_wrong').innerText = 
-                  parseInt(document.querySelector('.lesson__header_wrong').innerText) + 1;
-                alert("Неверно! Правильная фраза: \"" + currentPhraseKey + "\"");
-                document.querySelector('.test__phrase_en').innerText = '';
-                displayWords(currentPhraseKey);
-            }
-            loadNextPhrase(); 
-        }, 500); 
-    }
+          document.querySelector('#popup').style.display = 'none';
+        }, 1000);
+        document.querySelector('.test__phrase_en').innerText = '';
+      } else {
+        document.querySelector('.lesson__header_wrong').innerText =
+          parseInt(document.querySelector('.lesson__header_wrong').innerText) + 1;
+        alert("Неверно! Правильная фраза: \"" + currentPhraseKey + "\"");
+        document.querySelector('.test__phrase_en').innerText = '';
+        displayWords(currentPhraseKey);
+      }
+      loadNextPhrase();
+    }, 500);
+  }
 }
 
-// Инициализация
+function updateGoBackButton() {
+  const phraseDiv = document.querySelector('.test__phrase_en');
+  const goBackButton = document.querySelector('.test__ago');
+
+  // Disable the button if the phrase is empty
+  goBackButton.style.display = phraseDiv.innerText.trim() === '' ? 'none' : '';
+}
+
+// Event listener for "Назад" button click
+document.querySelector('.test__ago').onclick = goBack;
+
+// Инициализация 
 loadNextPhrase();
 
 
 
 
-
-
-
-
-
-
-
-
-$('ul.stages__tabs').on('click', 'li:not(.stages__tab_active)', function() {
+$('ul.stages__tabs').on('click', 'li:not(.stages__tab_active)', function () {
   $(this)
     .addClass('stages__tab_active').siblings().removeClass('stages__tab_active')
     .closest('div.container').find('div.stages__content').removeClass('stages__content_active').eq($(this).index()).addClass('stages__content_active');
@@ -99,7 +204,7 @@ const answerChoices = document.querySelectorAll('.stages__answerChoice');
 // Проходимся циклом по всем блокам с вариантами ответов
 answerChoices.forEach(choice => {
   // При нажатии на блок
-  choice.addEventListener('click', function() {
+  choice.addEventListener('click', function () {
     // Если блок имеет класс 'correctly', задний фон становится зеленым
     if (this.classList.contains('correctly')) {
       this.style.backgroundColor = 'green';
@@ -160,7 +265,7 @@ function dropHandler(event) {
   event.preventDefault();
   const draggableElementId = event.dataTransfer.getData('text/plain');
   const droppableElementId = event.target.getAttribute('data-draggable-id');
-  
+
   if (draggableElementId === droppableElementId) {
     const draggableElement = document.getElementById(draggableElementId);
     event.target.appendChild(draggableElement);
@@ -173,13 +278,13 @@ function dropHandler(event) {
 
 
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   const draggableElementContainer = document.querySelector('.draggable-elements');
   const draggableElement = Array.from(draggableElementContainer.children);
 
   // Создаем массив случайных индексов
   const randomIndexes = Array.from({ length: draggableElement.length }, (_, index) => index)
-      .sort(() => Math.random() - 0.5);
+    .sort(() => Math.random() - 0.5);
 
   // Создаем массив перемешанных блоков
   const shuffledElements = randomIndexes.map(index => draggableElement[index]);
@@ -189,7 +294,7 @@ window.addEventListener('load', function() {
 
   // Вставляем перемешанные блоки обратно в контейнер
   shuffledElements.forEach(element => {
-      draggableElementContainer.appendChild(element);
+    draggableElementContainer.appendChild(element);
   });
 });
 
@@ -204,13 +309,13 @@ var cards = container.querySelectorAll('.card');
 var cardsArray = Array.prototype.slice.call(cards);
 
 // Randomize the order of the cards
-cardsArray.sort(function() {
-    return 0.5 - Math.random();
+cardsArray.sort(function () {
+  return 0.5 - Math.random();
 });
 
 // Append the shuffled cards back into the container
-cardsArray.forEach(function(card) {
-    container.appendChild(card);
+cardsArray.forEach(function (card) {
+  container.appendChild(card);
 });
 
 
@@ -231,7 +336,7 @@ cardsArray.forEach(function(card) {
 
 
 
-       
+
 
 
 
