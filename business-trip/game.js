0bfabf68310712849bfcedb745c9c8dc93414848
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2025-01-27 14:05:16 +0300

    Add files via upload

---
+++ business-trip/game.js
@@ -0,0 +1,89 @@
+
+
+const words = {
+  "reach": "достигать",
+  "reference": "ссылка",
+  "response": "ответ",
+  "CV": "резюме",
+  "duty": "обязанности",
+  "description": "описание",
+  "title": "название",
+  "education": "образование",
+  "experience": "опыт",
+  "salary": "заработная плата",
+  "benefit": "выгода, прибыль",
+  "goal": "цель",
+  "resume": "резюме",
+  "responsibility": "ответственность",
+  "opportunity": "шанс",
+  "available": "доступный",
+  "position": "позиция",
+  "interview": "собеседование",
+  "strength": "сильная сторона",
+  "weakness": "слабость",
+  "impression": "впечатление",
+  "environment": "окружающая среда",
+  "compensation": "компенсация",
+  "package": "пакет",
+  "fit": "подходить",
+  "process": "процесс",
+  "attach": "прикреплять"
+};
+  
+  
+  
+  
+const keys = Object.keys(words);
+let currentKey;
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function updateContent() {
+  currentKey = getRandomElement(keys);
+  const correctValue = words[currentKey];
+
+  const enDiv = document.querySelector('.stages__item_content.stages__item_content_w100.stages__item_content_en');
+  enDiv.textContent = currentKey;
+
+  const allBrDivs = document.querySelectorAll('.stages__item_content.stages__item_content_w100.stages__item_content_br');
+
+  const randomIndex = Math.floor(Math.random() * allBrDivs.length);
+  allBrDivs[randomIndex].textContent = correctValue;
+
+  const values = Object.keys(words).filter(key => key !== currentKey).map(key => words[key]);
+  
+  allBrDivs.forEach((div, index) => {
+      if (index !== randomIndex) {
+          div.textContent = getRandomElement(values);
+      }
+      div.onclick = () => checkAnswer(div, div.textContent);
+  });
+}
+
+function checkAnswer(div, selectedValue) {
+  const correctCountDiv = document.querySelector('.lesson__header_right');
+  const wrongCountDiv = document.querySelector('.lesson__header_wrong');
+  const popup = document.getElementById('popup');
+  const popupMessage = document.getElementById('popupMessage');
+
+  if (selectedValue === words[currentKey]) {
+      correctCountDiv.textContent = parseInt(correctCountDiv.textContent) + 1; // Increment correct answers
+     
+      popup.style.display = 'block'; // Show popup
+      div.style.backgroundColor = "#008000"; 
+      setTimeout(() => {
+          div.style.backgroundColor = ""; 
+          popup.style.display = 'none'; // Hide popup
+          updateContent(); 
+      }, 1500);
+  } else {
+      wrongCountDiv.textContent = parseInt(wrongCountDiv.textContent) + 1; // Increment wrong answers
+      div.style.backgroundColor = "#ff0000";
+      setTimeout(() => div.style.backgroundColor = "", 500);
+  }
+}
+
+// Initial call to set content
+updateContent();
