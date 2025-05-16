585b0fbacba6f3423df00130bc26a98dc36dee0f
MyEnglish1 <149169953+MyEnglish1@users.noreply.github.com>
2025-05-16 13:58:05 +0300

    Add files via upload

---
+++ health/game.js
@@ -0,0 +1,96 @@
+
+
+const words = {
+  "cigarette": "сигарета",
+  "exercise": "тренироваться",
+  "feel": "чувствовать",
+  "flu": "грипп",
+  "healthy": "здоровый",
+  "relax": "отдыхать",
+  "sick": "больной",
+  "smell": "пахнуть",
+  "smoke": "курить",
+  "sound": "звучать",
+  "taste": "попробовать",
+  "about": "приблизительно",
+  "day": "день",
+  "every": "каждый",
+  "month": "месяц",
+  "once": "один раз",
+  "shape": "форма, очертание",
+  "time": "время",
+  "twice": "дважды",
+  "week": "неделя",
+  "year": "год",
+  "aspirin": "аспирин",
+  "cough": "кашлять",
+  "drink": "пить",
+  "fever": "жар, лихорадка",
+  "get": "получить",
+  "headache": "головная боль",
+  "hurt": "делать больно",
+  "liquid": "жидкость",
+  "sore": "больной",
+  "symptom": "симптом",
+  "take": "принимать, принять",
+  "tired": "усталый",
+  "wrong": "неподходящий"
+};
+
+
+
+
+const keys = Object.keys(words);
+let currentKey;
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function updateContent() {
+  currentKey = getRandomElement(keys);
+  const correctValue = words[currentKey];
+
+  const enDiv = document.querySelector('.stages__item_content.stages__item_content_w100.stages__item_content_en');
+  enDiv.textContent = currentKey;
+
+  const allBrDivs = document.querySelectorAll('.stages__item_content.stages__item_content_w100.stages__item_content_br');
+
+  const randomIndex = Math.floor(Math.random() * allBrDivs.length);
+  allBrDivs[randomIndex].textContent = correctValue;
+
+  const values = Object.keys(words).filter(key => key !== currentKey).map(key => words[key]);
+
+  allBrDivs.forEach((div, index) => {
+    if (index !== randomIndex) {
+      div.textContent = getRandomElement(values);
+    }
+    div.onclick = () => checkAnswer(div, div.textContent);
+  });
+}
+
+function checkAnswer(div, selectedValue) {
+  const correctCountDiv = document.querySelector('.lesson__header_right');
+  const wrongCountDiv = document.querySelector('.lesson__header_wrong');
+  const popup = document.getElementById('popup');
+  const popupMessage = document.getElementById('popupMessage');
+
+  if (selectedValue === words[currentKey]) {
+    correctCountDiv.textContent = parseInt(correctCountDiv.textContent) + 1; // Increment correct answers
+
+    popup.style.display = 'block'; // Show popup
+    div.style.backgroundColor = "#008000";
+    setTimeout(() => {
+      div.style.backgroundColor = "";
+      popup.style.display = 'none'; // Hide popup
+      updateContent();
+    }, 1500);
+  } else {
+    wrongCountDiv.textContent = parseInt(wrongCountDiv.textContent) + 1; // Increment wrong answers
+    div.style.backgroundColor = "#ff0000";
+    setTimeout(() => div.style.backgroundColor = "", 500);
+  }
+}
+
+// Initial call to set content
+updateContent();
